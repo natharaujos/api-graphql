b098cafadfae766b9bdeb1f614dd6a8b539cd9fa
Nathan Araujo <nathan.araujo@dtidigital.com.br>
2025-02-26 10:24:34 -0300

    feat: finishing endpoints to create a customer

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -9,9 +9,17 @@ import {
 import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
 import { Appointment } from "../dtos/models/appointment-model";
 import { Customer } from "../dtos/models/cutomer-model";
+import { CreateCustomerInput } from "../dtos/inputs/create-customer-input";
+import { PrismaClient } from "@prisma/client";
 
 @Resolver(() => Appointment)
 export class AppointmentsResolver {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
   @Query(() => [Appointment])
   async appointments() {
     return [
@@ -22,12 +30,26 @@ export class AppointmentsResolver {
     ];
   }
 
+  @Mutation(() => Customer)
+  async createCustomer(@Arg("data") data: CreateCustomerInput) {
+    const customer = {
+      name: data.name,
+    };
+
+    await this.prisma.customer.create({ data: customer });
+
+    return customer;
+  }
+
   @Mutation(() => Appointment)
   async createAppointment(@Arg("data") data: CreateAppointmentInput) {
     const appointment = {
       startsAt: data.startsAt,
       endsAt: data.endsAt,
+      customerId: data.customerId,
     };
+
+    await this.prisma.appointment.create({ data: appointment });
 
     return appointment;
   }
